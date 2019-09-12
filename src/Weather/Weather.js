7a6f612e992db25664e97e727af95379d99f05b9
Paul Negoescu <paul.negoescu@iquestgroup.com>
2019-09-12 19:55:54 +0300

    Refactorare Weather cu clasa

--- src/Weather/Weather.js
+++ src/Weather/Weather.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Weather() {
-  const [temperature, setTemperature] = useState('Loading ...');
+class Weather extends React.Component {
+  state = {
+    temperature: 'Loading ...',
+    alta: 'test'
+  };
 
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=Brasov,ro&appid=c7da641777760054e5ca6164eb47580a')
-    .then( res => res.json() )
-    .then( data => setTemperature((data.main.temp - 273.15).toFixed(1)) );
+  async componentDidMount() {
+    const data = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Brasov,ro&appid=c7da641777760054e5ca6164eb47580a')
+    .then( res => res.json() );
 
+    console.log(data.name)
+    this.setState({ 
+      temperature: (data.main.temp - 273.15).toFixed(1) 
+    });
+  }
 
-  return (
-    <p>
-      <span>Temperature for Brasov: </span>
-      <strong>{ temperature } &deg;C</strong>
-    </p>
-  );
+  render() {
+    return (
+      <p>
+        <span>Temperature for Brasov { this.state.alta }: </span>
+        <strong>{ this.state.temperature } &deg;C</strong>
+      </p>
+    );
+  }
 }
 
 export default Weather;
