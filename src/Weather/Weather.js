3a87ae4c0d69754313436434d8f8beb7c4bab6db
Paul Negoescu <paul.negoescu@iquestgroup.com>
2019-09-12 19:28:04 +0300

    Cod Weather functional cu then-uri

--- src/Weather/Weather.js
+++ src/Weather/Weather.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Weather() {
-  const temperature = 'Hello from Weather component';
+  const [temperature, setTemperature] = useState('Loading ...');
 
   fetch('https://api.openweathermap.org/data/2.5/weather?q=Brasov,ro&appid=c7da641777760054e5ca6164eb47580a')
-    .then(res => res.json())
-    .then(data => console.log(data));
+    .then( res => res.json() )
+    .then( data => setTemperature((data.main.temp - 273.15).toFixed(1)) );
+
 
   return (
     <p>
       <span>Temperature for Brasov: </span>
-      <strong>{ temperature }</strong>
+      <strong>{ temperature } &deg;C</strong>
     </p>
   );
 }
